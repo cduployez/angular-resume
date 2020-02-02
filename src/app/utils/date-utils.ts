50c3fdcf51987ba72f1655829b47635614002f51
cduployez <60548405+cduployez@users.noreply.github.com>
2020-02-02 17:45:45 +0100

    HeaderComponent info

--- src/app/utils/date-utils.ts
+++ src/app/utils/date-utils.ts
@@ -4,4 +4,18 @@ export class DateUtils {
         return new Date(year, 0, 1, 0, 0, 0, 0);
     }
 
+    /**
+     * Source : https://stackoverflow.com/a/21984136
+     * @param birthday Date d'anniversaire
+     */
+    static toAge(birthday: Date): number {
+        const dateDiff: number = Date.now() - birthday.getTime();
+        const ageDate: Date = new Date(dateDiff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    static fromDate(day: number, month: number, year: number): Date {
+        return new Date(year, month - 1, day, 0, 0, 0, 0);
+    }
+
 }
