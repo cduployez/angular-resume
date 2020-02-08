f3c7619c66596a5547d15f535bfdcf4475c03bca
cduployez <60548405+cduployez@users.noreply.github.com>
2020-02-08 17:33:30 +0100

    HobbiesComponent content & HTML

--- src/app/components/hobbies/hobbies.component.ts
+++ src/app/components/hobbies/hobbies.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HobbyItem} from '../../model/hobby/hobby-item';
 
 @Component({
-  selector: 'cv-hobbies',
-  templateUrl: './hobbies.component.html',
-  styleUrls: ['./hobbies.component.scss']
+    selector: 'cv-hobbies',
+    templateUrl: './hobbies.component.html',
+    styleUrls: ['./hobbies.component.scss']
 })
 export class HobbiesComponent implements OnInit {
 
-  constructor() { }
+    hobbyItems: HobbyItem[] = HobbiesComponent.initHobbyItems();
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    private static initHobbyItems(): HobbyItem[] {
+        return [
+            new HobbyItem('Films et séries', ['Inception', 'Le Roi Lion', 'Kaamelott'], true),
+            new HobbyItem('Musique', ['Pratique de la guitare', 'chansons françaises à texte']),
+            new HobbyItem('Sport', ['Natation'])
+        ];
+    }
+
+    ngOnInit(): void {
+    }
 }
