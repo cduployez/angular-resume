c28cf338161588fe8a7f8b63e57e4b31fc2022e2
cduployez <60548405+cduployez@users.noreply.github.com>
2020-02-08 16:55:59 +0100

    LanguagesComponent content

--- src/app/components/languages/languages.component.ts
+++ src/app/components/languages/languages.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LanguageItem} from '../../model/language/language-item';
+import {DateUtils} from '../../utils/date-utils';
+import {CvDate} from '../../model/shared/cv-date';
 
 @Component({
-  selector: 'cv-languages',
-  templateUrl: './languages.component.html',
-  styleUrls: ['./languages.component.scss']
+    selector: 'cv-languages',
+    templateUrl: './languages.component.html',
+    styleUrls: ['./languages.component.scss']
 })
 export class LanguagesComponent implements OnInit {
 
-  constructor() { }
+    languageItems: LanguageItem[] = LanguagesComponent.initLanguageItems();
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    private static initLanguageItems(): LanguageItem[] {
+        return [
+            new LanguageItem(
+                new CvDate(DateUtils.fromYear(2017), DateUtils.fromYear(2017), 'year'),
+                'TOEIC',
+                'Score : 975'
+            ),
+            new LanguageItem(
+                new CvDate(DateUtils.fromYear(2011), DateUtils.fromYear(2011), 'year'),
+                'First Certificate in English',
+                'Grade A, Niveau Européen C1'
+            )
+        ];
+    }
+
+    ngOnInit(): void {
+    }
 }
