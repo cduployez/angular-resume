0746280b54285e296b34ac21244bd11506b8de8e
cduployez <60548405+cduployez@users.noreply.github.com>
2020-02-08 17:48:23 +0100

    ProjectsComponent content & HTML

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ProjectItem} from '../../model/project/project-item';
 
 @Component({
     selector: 'cv-projects',
@@ -7,7 +8,29 @@ import {Component} from '@angular/core';
 })
 export class ProjectsComponent {
 
+    projectItems: ProjectItem[] = ProjectsComponent.initProjectItems();
+
     constructor() {
     }
 
+    private static initProjectItems(): ProjectItem[] {
+        return [
+            new ProjectItem('TwinMax', 5,
+                ['Application Android de calcul et affichage de courbes en temps réel afin de calibrer les carburateurs d’une moto pour l’entreprise Selectronic'],
+                ['Java', 'Android']
+            ),
+            new ProjectItem('MatchFM', 4,
+                ['Application Android de rencontre de personnes autour des recommandations musicales'],
+                ['C#', 'Xamarin', '.NET']
+            ),
+            new ProjectItem('Rubik Pixel Art Solver', 5,
+                ['Robot permettant de scanner et manipuler des Rubik’s Cubes non résolus et former des images'],
+                ['C++', 'OpenCV']
+            ),
+            new ProjectItem('Traveling Bradeux Problem Solver', 3,
+                ['Planification de tournées sur Google Maps'],
+                ['HTML', 'CSS', 'JavaScript', 'Google Maps', 'C++']
+            )
+        ];
+    }
 }
