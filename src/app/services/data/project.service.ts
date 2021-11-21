00b8a57953d551c17c272d188833f0cdf99921dd
cduployez <60548405+cduployez@users.noreply.github.com>
2021-11-21 15:21:48 +0100

    Changement de la date de révision

--- src/app/services/data/project.service.ts
+++ src/app/services/data/project.service.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class ProjectService {
 
-    readonly latestRevisionYear: number = 2020;
+    readonly latestRevisionYear: number = 2021;
 
 }
