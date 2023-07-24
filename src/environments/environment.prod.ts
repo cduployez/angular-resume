bac939ae6b4b1da4e2797e10e25d1c2713e5d7ac
cduployez <c.duployez@gmail.com>
2023-07-24 13:20:58 +0200

    Adapt environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-import { IEnvironment } from "../app/model/config/ienvironment";
+import {IEnvironment} from '../app/model/config/ienvironment';
 
 export const environment: IEnvironment = {
-  production: true,
-  backResumeUrl: "http://localhost:10001",
+  production: true
 };
